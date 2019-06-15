a780b1e41283e6f66711838390b46d5334aed52c
Josh Wolfley <josh.wolfley@gmail.com>
2019-06-14 20:42:59 -0400

    Create Store

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from  'redux-thunk';
+import { Provider } from 'react-redux'
+import AssetReducer from './reducers/asset'
+
 import './index.css';
 import Balancesheet from './containers/Balancesheet'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Balancesheet />, document.getElementById('root'));
+const store = createStore(
+    AssetReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Balancesheet />
+    </Provider>
+    ,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
