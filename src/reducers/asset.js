b1c991c893f1bbe32d97e0caed0d2050781aa401
Josh Wolfley <josh.wolfley@gmail.com>
2019-06-14 20:21:31 -0400

    create ADD_ASSET reducer

--- src/reducers/asset.js
+++ src/reducers/asset.js
@@ -1,23 +1,43 @@
 import * as AssetActionTypes from '../actiontypes/asset.js';
 
-const initialState = [
-    {
-        account_name: 'Cash',
-        amount: 300.32,
-    },
-    {
-        account_name: 'A/R',
-        amount: 20.89,
-    },
-    {
-        account_name: 'Inventory',
-        amount: 500,
-    },
-];
+const initialState = {
+    assets: [
+        {
+            account_name: 'Cash',
+            amount: 300.32,
+        }
+        ,
+        {
+            account_name: 'A/R',
+            amount: 20.89,
+        }
+        ,
+        {
+            account_name: 'Inventory',
+            amount: 500,
+        }
+        ,
+    ]
+
+};
+
+
 
 export default function Asset(state=initialState, action){
 
+    switch (action.type) {
 
+        case AssetActionTypes.ADD_ASSET: {
+            return Object.assign({}, state, {
+                assets: [...state.assets, {
+                    account_name: action.name,
+                    amount: action.amount
+                }]
+            })
+        }
 
+        default:
+            return state
+    }
 }
 
